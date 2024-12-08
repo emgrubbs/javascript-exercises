24954a919dce20baba9d77700b6c2191c1c088a0
Elizabeth_G <173961000+emgrubbs@users.noreply.github.com>
2024-12-07 21:29:01 -0600

    Complete assignment.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+   const arr = Array.from(str);
+   const rev = arr.reverse();
+   return rev.join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
