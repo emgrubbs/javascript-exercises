a5b55fb4cbbfa59df3aaefba422d7bb559c6deeb
Elizabeth_G <173961000+emgrubbs@users.noreply.github.com>
2024-12-10 13:55:02 -0600

    Complete Assignemnt.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+      let sum = 0;
+      let low = 0;
+      let high = 0;
+      function lowToHigh(a, b) {
+         if (x > y) {
+         high += x;
+         low += y;
+      }
+        else {
+         high += y;
+         low += x;
+      };
+      }
+      if ((Number.isInteger(x) && x > 0) && (Number.isInteger(y) && y > 0)) {
+         lowToHigh(x, y);
+      }
+      else {
+            return "ERROR";
+      }
+      let i = low;
+      while (i <= high)  {
+         sum += i;
+         i++;
+      }
+      return sum;
+      };
+   
 
 // Do not edit below this line
 module.exports = sumAll;
