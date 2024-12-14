7d3e3f11b38f54a16dd2d60fb436b9d514366340
Elizabeth_G <173961000+emgrubbs@users.noreply.github.com>
2024-12-13 20:12:07 -0600

    Complete Assignment.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(Fahr) {
+   let Cels = ((Fahr - 32) * (5 / 9));
+   let roundedCelsString = Cels.toFixed(1);
+   let roundedCelsNum = parseFloat(roundedCelsString);
+   return roundedCelsNum;
+   };
 
-const convertToFahrenheit = function() {
-};
+
+const convertToFahrenheit = function(Cels) {
+   let Fahr = ((Cels * (9 / 5)) + 32);
+   let roundedFahrString = Fahr.toFixed(1);
+   let roundedFahrNum = parseFloat(roundedFahrString);
+   return roundedFahrNum; 
+   };
 
 // Do not edit below this line
 module.exports = {
